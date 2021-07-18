fec1e722945bd4a037ff17c6e79949d819552c36
Balázs Bognár <balage4@hotmail.com>
2021-07-18 23:34:50 +0200

    create user state at App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Registration from './components/registration/Registration';
 import './scss/global.scss';
 
 function App() {
+
+  function getUser() {
+    return localStorage.getItem('user');
+  }
+
+  const [user, setUser] = useState(getUser());
+
+
   return (
     <>
       <Router>
         <Switch>
+          <Route path="/dashboard">
+            <Dashboard
+              user={user}
+              setUser={setUser}
+            />
+          </Route>
           <Route path="/register">
             <Registration />
           </Route>
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,4 +1,5 @@
-export default function Dashboard() {
+// eslint-disable-next-line react/prop-types
+export default function Dashboard({ user, setUser }) {
   return (
     <div>
       Dashboard
