d72936d06996d4d61e13c77d8f3860cbf71fe840
Balázs Bognár <balage4@hotmail.com>
2021-08-14 18:21:29 +0200

    listOfEvents minor frontend fixes

--- frontend/src/components/dashboard/users/Events/EventsTable.js
+++ frontend/src/components/dashboard/users/Events/EventsTable.js
@@ -6,9 +6,9 @@ export default function EventsTable({ listOfEvents }) {
   const headerMap = [
     ['_id', 'ID'],
     ['userName', 'Felhasználó'],
-    ['eventDate', 'Date'],
-    ['eventService', 'Service'],
-    ['eventProvider', 'Provider']
+    ['eventDate', 'Dátum'],
+    ['eventService', 'Szolgáltatás'],
+    ['eventProvider', 'Szolgáltató neve']
   ];
 
   return (
