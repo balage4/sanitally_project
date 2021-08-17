7e80a229bb05931a4e496baccf5ce3d04c98be78
Balázs Bognár <balage4@hotmail.com>
2021-08-17 21:27:34 +0200

    frontend styling in admin-menu

--- frontend/src/components/admin/Admin.js/AdminMain.js
+++ frontend/src/components/admin/Admin.js/AdminMain.js
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 import { Link, Redirect } from "react-router-dom"
 import AuthenticatedNavbar from "../../navbars/authenticatedNavbar/AuthenticatedNavbar"
+import usersIcon from '../../../assets/usersIcon.png';
+import medicalServicesIcon from '../../../assets/medicalServicesIcon.png';
+import categoriesIcon from '../../../assets/categoriesIcon.png';
+import '../../../scss/admin.scss';
 
 export default function AdminMain({ user, setUser }) {
 
@@ -14,19 +18,24 @@ export default function AdminMain({ user, setUser }) {
     <div className="admin text-center">
       <AuthenticatedNavbar user={user} setUser={setUser} />
       <h2>Admin menü</h2>
-      <div className="m-3">
+      <div className="admin-menu">
         <Link
-          className="btn btn-primary m-3"
-          to="/admin/users" >Felhasználók listázása
+          className="admin-link-item"
+          to="/admin/users">Felhasználók listázása
+          <img src={usersIcon} alt="users" width="150" />
         </Link>
+
         <Link
-          className="btn btn-secondary m-3"
+          className="admin-link-item"
           to="/admin/services" >Szolgáltatások listázása
+          <img src={medicalServicesIcon} alt="users" width="150" />
         </Link>
         <Link
-          className="btn btn-info m-3"
+          className="admin-link-item"
           to="/admin/categories" >Főoldali lista szerkesztése
+          <img src={categoriesIcon} alt="users" width="150" />
         </Link>
+
       </div>
 
     </div>
