93c1affd2e882f0cd422f2080b7970fdc7f50ca4
Balázs Bognár <balage4@hotmail.com>
2021-08-23 00:14:34 +0200

    upgrade preview style

--- frontend/src/components/Provider/Prescriptions/PrintPreview.js
+++ frontend/src/components/Provider/Prescriptions/PrintPreview.js
@@ -4,17 +4,44 @@ import '../../../scss/printPreview.scss';
 
 export default function PrintPreview({ user }) {
 
-  const { provider } = useParams();
+  const { provider, vaccine, dosage, date } = useParams();
 
   return (
     <div className="book">
       <div className="page">
-        Hello {provider}
-        Bello{user.firstName}
-        <div className="subpage">Page 1/2</div>
-      </div>
-      <div className="page">
-        <div className="subpage">Page 2/2</div>
+        <div className="subpage">
+          <h3>SanitAlly recept</h3>
+          <table className="print-table">
+            <thead>
+              <tr>
+                <th>Kiállítás dátuma</th>
+                <th>Recept kiállítója</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{date}</td>
+                <td>{provider}</td>
+              </tr>
+            </tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th>Páciens neve</th>
+                <th>Gyógyszer neve</th>
+                <th>Előírt adagolás</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{`${user.lastName} ${user.firstName}`}</td>
+                <td>{vaccine}</td>
+                <td>{dosage}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
 
