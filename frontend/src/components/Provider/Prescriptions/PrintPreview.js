01d818cb5c5a0985356c7b113bfb481ab10cf84d
Balázs Bognár <balage4@hotmail.com>
2021-08-22 23:50:54 +0200

    create printable html view

--- frontend/src/components/Provider/Prescriptions/PrintPreview.js
+++ frontend/src/components/Provider/Prescriptions/PrintPreview.js
@@ -1,32 +1,22 @@
 /* eslint-disable react/prop-types */
 import { useParams } from 'react-router-dom';
-import '../../../scss/preview.scss';
+import '../../../scss/printPreview.scss';
 
 export default function PrintPreview({ user }) {
 
-  const { provider, vaccine, dosage, date } = useParams();
+  const { provider } = useParams();
 
   return (
-    <div className="m-3 p-3 border border-dark col-10 m-auto">
-
-      <h2 className="m-3">SanitAlly recept</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Recept kiállítója</th>
-            <th>Beteg neve</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{provider}</td>
-            <td>{`${user.lastName} ${user.firstName}`}</td>
-            <td>{vaccine}</td>
-            <td>{dosage}</td>
-            <td>{date}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="book">
+      <div className="page">
+        Hello {provider}
+        Bello{user.firstName}
+        <div className="subpage">Page 1/2</div>
+      </div>
+      <div className="page">
+        <div className="subpage">Page 2/2</div>
+      </div>
     </div>
+
   )
 }
