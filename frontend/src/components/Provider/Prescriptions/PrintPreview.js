789e403262000a942127d005255467bc8665b4f1
Balázs Bognár <balage4@hotmail.com>
2021-08-23 00:35:30 +0200

    prescription preview: minor changes in style

--- frontend/src/components/Provider/Prescriptions/PrintPreview.js
+++ frontend/src/components/Provider/Prescriptions/PrintPreview.js
@@ -5,45 +5,50 @@ import '../../../scss/printPreview.scss';
 export default function PrintPreview({ user }) {
 
   const { provider, vaccine, dosage, date } = useParams();
+  const actualDate = new Date();
 
   return (
-    <div className="book">
-      <div className="page">
-        <div className="subpage">
-          <h3>SanitAlly recept</h3>
-          <table className="print-table">
-            <thead>
-              <tr>
-                <th>Kiállítás dátuma</th>
-                <th>Recept kiállítója</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{date}</td>
-                <td>{provider}</td>
-              </tr>
-            </tbody>
-          </table>
-          <table>
-            <thead>
-              <tr>
-                <th>Páciens neve</th>
-                <th>Gyógyszer neve</th>
-                <th>Előírt adagolás</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{`${user.lastName} ${user.firstName}`}</td>
-                <td>{vaccine}</td>
-                <td>{dosage}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+    <div className="page">
+      <div className="subpage">
+        <h3 className="mt-5">SanitAlly recept</h3>
+        <table className="print-table">
+          <thead>
+            <tr>
+              <th>Kiállítás dátuma</th>
+              <th>Recept kiállítója</th>
+              <th>Páciens neve</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{date}</td>
+              <td>{provider}</td>
+              <td>{`${user.lastName} ${user.firstName}`}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h6>
+          Önnek az alábbi gyógykészítmény kiváltására van jogosultsága:
+        </h6>
+        <table className="print-table">
+          <thead>
+            <tr>
+              <th>Gyógyszer neve</th>
+              <th>Előírt adagolás</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{vaccine}</td>
+              <td>{dosage}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="signature">______________________________</div>
+        <p>Beteg aláírása ({`${user.lastName} ${user.firstName}`})</p>
+        <div />
       </div>
+      <div className="foot">SanitAlly Copyright {actualDate.getFullYear()}.</div>
     </div>
-
   )
 }
